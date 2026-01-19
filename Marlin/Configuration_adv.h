66862eef2bfa38b659aa6a993a25bda2b4dad3a4
cristomoya <tu-email@example.com>
2026-01-19 19:18:58 +0100

    subido motor a 850/16

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2872,7 +2872,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      650
+    #define E0_CURRENT      850
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
